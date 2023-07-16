a7d1a6a4e4e8885cc9cccbd738019fdc1fb7af58
Eduardo <eduardocasagrande71@gmail.com>
2023-07-16 18:59:47 -0300

    Create characters page

--- src/router/routes.js
+++ src/router/routes.js
@@ -2,7 +2,15 @@ const routes = [
   {
     path: "/",
     component: () => import("layouts/HomeLayout.vue"),
-    children: [{ path: "", component: () => import("pages/IndexPage.vue") }],
+    children: [{ path: "", component: () => import("pages/HomePage.vue") }],
+  },
+
+  {
+    path: "/character",
+    component: () => import("layouts/CharacterLayout.vue"),
+    children: [
+      { path: ":id", component: () => import("pages/CharacterPage.vue") },
+    ],
   },
 
   {
