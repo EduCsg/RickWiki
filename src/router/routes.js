8ab23780ef11fba2ad6460f0c126b1f8631f1025
Eduardo <eduardocasagrande71@gmail.com>
2023-07-17 20:41:23 -0300

    Fix characters router filter

--- src/router/routes.js
+++ src/router/routes.js
@@ -26,7 +26,31 @@ const routes = [
     path: "/character",
     component: () => import("layouts/HomeLayout.vue"),
     children: [
-      { path: ":id", component: () => import("pages/CharacterPage.vue") },
+      {
+        path: ":id",
+        component: () => import("pages/CharacterPage.vue"),
+
+        beforeEnter: (to, _, next) => {
+          const characterId = Number(to.params.id);
+
+          if (!characterId || characterId < 1 || characterId > 826) {
+            next({ path: "/character/1" });
+
+            return;
+          }
+
+          next();
+        },
+      },
+    ],
+  },
+
+  {
+    path: "/episodes",
+    component: () => import("layouts/HomeLayout.vue"),
+    children: [
+      { path: "", component: () => import("pages/EpisodesPage.vue") },
+      // { path: ":id", component: () => import("pages/EpisodesPage.vue") },
     ],
   },
 
